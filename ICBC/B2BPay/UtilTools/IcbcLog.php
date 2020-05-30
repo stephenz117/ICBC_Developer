7e736d0411278e5ed8bf12a8bcfc6028f520b18e
stephenz117 <stephenz117@126.com>
2020-05-30 19:14:14 +0800

    system optimization

--- ICBC/B2BPay/UtilTools/IcbcLog.php
+++ ICBC/B2BPay/UtilTools/IcbcLog.php
@@ -9,12 +9,12 @@ class IcbcLog
 		$logdir = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'logs';
 		if (!is_dir($logdir))
 		{
-			mkdir($logdir, '0777');
+			mkdir($logdir);
 		}
 		$logfile = $logdir . DIRECTORY_SEPARATOR . date('Y-m-d') . '.log';
-		$filecontent = date('Y-m-d H:i:s') . ' --- ' . $content['biz_content']['partnerSeq'] . PHP_EOL . 
-			' Headers --- ' . $content['biz_content']['partnerSeq'] . ' : ' . json_encode($headers) . PHP_EOL . 
-			' Content --- ' . $content['biz_content']['partnerSeq'] . ' : ' . json_encode($content) . PHP_EOL;
+		$filecontent = date('Y-m-d H:i:s') . ' --- ' . $content['msg_id'] . PHP_EOL . 
+			' Headers --- ' . $content['msg_id'] . ' : ' . json_encode($headers) . PHP_EOL . 
+			' Content --- ' . $content['msg_id'] . ' : ' . json_encode($content) . PHP_EOL;
 		@file_put_contents($logfile, $filecontent, FILE_APPEND);
 		return true;
 	}
